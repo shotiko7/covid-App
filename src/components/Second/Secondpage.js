9caf84bcc683365bf9120817978745d640de0327
Shota <shotikoavaliani97@gmail.com>
2022-08-20 18:03:19 +0400

    second page is done

--- src/components/Second/Secondpage.js
+++ src/components/Second/Secondpage.js
@@ -14,7 +14,8 @@ function Secondpage(props) {
             <Form onClick={props.onClick} />
         </div>
         <div className='secondright'>
-
+            <h2> Redberry Origins </h2>
+            <p> You watch “What? Where? When?” Yeah. Our founders used to play it. That’s where they got a question about a famous American author and screenwriter Ray Bradbury. Albeit, our CEO Gaga Darsalia forgot the exact name and he answered Ray Redberry. And at that moment, a name for a yet to be born company was inspired - Redberry 😇 </p> 
         </div>
     </div>
   )
